783abb74af5ede73e242acde49b1b7190916db6d
DanianiWong-web <75284535+DanianiWong-web@users.noreply.github.com>
2021-06-17 15:49:21 -0700

    Add files via upload

---
+++ StudentDatabaseApp.java
@@ -0,0 +1,15 @@
+package studentdatabase;
+
+public class StudentDatabaseApp {
+	
+	public static void main(String[] args) {
+		Student stu1 = new Student();
+		stu1.enroll();
+		stu1.payTuition();
+		System.out.println(stu1.toString());
+		//Ask how many new users we want to add 
+		
+		//Create a number for new student
+	}
+
+}
